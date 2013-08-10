aaa69ebc9091aa69352f896a6deb378831f21c68
CB_27 <Chris.B@outlook.com>
2013-08-10 11:09:25 -0400

    1.1.2 Final

--- WeatherLock/SettingsPivot.xaml.cs
+++ WeatherLock/SettingsPivot.xaml.cs
@@ -1,4 +1,4 @@
-﻿#define DEBUG_AGENT
+﻿//#define DEBUG_AGENT
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -581,9 +581,9 @@ namespace WeatherLock
                 ScheduledActionService.Add(periodicTask);
                 PeriodicStackPanel.DataContext = periodicTask;
 
-#if(DEBUG_AGENT)
-                ScheduledActionService.LaunchForTest(periodicTaskName, TimeSpan.FromSeconds(10));
-#endif
+//#if(DEBUG_AGENT)
+                //ScheduledActionService.LaunchForTest(periodicTaskName, TimeSpan.FromSeconds(10));
+//#endif
             }
             catch (InvalidOperationException exception)
             {
@@ -678,7 +678,7 @@ namespace WeatherLock
         private void updateData()
         {
             //Testing Key
-            apiKey = "fb1dd3f4321d048d";
+            //apiKey = "fb1dd3f4321d048d";
 
             checkLocation();
             checkUnits();
