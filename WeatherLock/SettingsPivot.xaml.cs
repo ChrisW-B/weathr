20e193485703f9bb613f428f34e61e17b2a5ad1a
CB_27 <Chris.B@outlook.com>
2013-08-22 18:26:29 -0400

    Turned off debug stuff

--- WeatherLock/SettingsPivot.xaml.cs
+++ WeatherLock/SettingsPivot.xaml.cs
@@ -1,4 +1,4 @@
-﻿#define DEBUG_AGENT
+﻿//#define DEBUG_AGENT
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -831,9 +831,9 @@ namespace WeatherLock
             {
                 ScheduledActionService.Add(periodicTask);
                 PeriodicStackPanel.DataContext = periodicTask;
-                #if(DEBUG_AGENT)
-                ScheduledActionService.LaunchForTest(periodicTaskName, TimeSpan.FromSeconds(10));
-                #endif
+               // #if(DEBUG_AGENT)
+               // ScheduledActionService.LaunchForTest(periodicTaskName, TimeSpan.FromSeconds(10));
+               // #endif
 
             }
             catch (InvalidOperationException exception)
