9ded9232921b944f7500d3ba02e0a4f777083fba
CB_27 <Chris.B@outlook.com>
2013-11-14 08:11:48 -0500

    1.3 Final

--- WeatherLock/SettingsPivot.xaml.cs
+++ WeatherLock/SettingsPivot.xaml.cs
@@ -75,7 +75,7 @@ namespace WeatherLock
         {
             InitializeComponent();
             //Testing Key
-            apiKey = "fb1dd3f4321d048d";
+            //apiKey = "fb1dd3f4321d048d";
             SystemTray.Opacity = .5;
         }
 
